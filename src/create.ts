8f17ce626c4a2913ab3d3e53645272b8f27d70a1
Syed Faraaz Ahmad <faraaz98@live.com>
2019-07-02 16:32:16 +0530

    refactor into separate functions

--- src/create.ts
+++ src/create.ts
@@ -1,5 +1,7 @@
 import { join, resolve } from 'path';
 import { exec } from 'child_process';
+import { existsSync } from 'fs';
+
 
 import chalk from 'chalk';
 import { CommandModule, Arguments, CommandBuilder, Argv } from 'yargs';
@@ -32,25 +34,54 @@ export default class Create implements CommandModule {
         }
 
         this.handler = async (args: Arguments) => {
+            /* 
+                check if ~/.floyd/ exists
+                !! Callback version used since fs.promises.access is still experimental as of July 2nd, 2019 !!
+            */
+            let floydExists = existsSync(join(`${process.env.HOME}`, '.floyd'));
+            let appExists: boolean;
+
+            if (floydExists) {
+                appExists = existsSync(join(`${process.env.HOME}`, '.floyd', 'app'));
+            } else {
+
+            }
+            // if not, create ~/.floyd and clone app into it
+
+
+
+
+
+
+
+
             let name: string = args.name as string;
             let path: string = args.path as string;
 
-            // replace '~' with /home/<username>
-            if (path[0] == '~') {
-                path = join(<string>process.env.HOME, path.slice(1));
-            }
-
-            // get the absolute path and add project path to it
-            let absolutePath = resolve(path);
-            absolutePath = join(absolutePath, name);
-
-            let command = `git clone https://github.com/floyd-framework/app.git ${absolutePath}`;
-            exec(command, (err) => {
-                // error encountered while executing command
-                if (err) {
-                    console.error(chalk.red(err.message));
-                }
-            });
+            
         };
-    }   
+    }
+    
+    private clone(name: string, path: string) {
+        // replace '~' with /home/<username>
+        if (path[0] == '~') {
+            path = join(<string>process.env.HOME, path.slice(1));
+        }
+
+        // get the absolute path and add project path to it
+        let absolutePath = resolve(path);
+        absolutePath = join(absolutePath, name);
+
+        let command = `git clone https://github.com/floyd-framework/app.git ${absolutePath}`;
+        exec(command, (err) => {
+            // error encountered while executing command
+            if (err) {
+                console.error(chalk.red(err.message));
+            }
+        });
+    }
+
+    private copyToPath(path: string) {
+
+    }
 };
