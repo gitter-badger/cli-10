657633ecb502b680eec99044752317f41f406fbd
Syed Faraaz Ahmad <faraaz98@live.com>
2019-07-06 19:08:03 +0530

    finish 2 tasks from add command

--- src/add.ts
+++ src/add.ts
@@ -2,13 +2,10 @@
     CLI command to add new component to floyd project folder
 */
 
-import { join, resolve } from 'path';
-import { exec } from 'child_process';
-import { rmdir, readdirSync } from 'fs';
-
-import chalk from 'chalk';
-import moment from 'moment';
 import { CommandModule, Arguments, CommandBuilder, Argv } from 'yargs';
+import { writeFile } from 'fs';
+import { join } from 'path';
+import chalk from 'chalk';
 
 export default class Add implements CommandModule {
     public command: string;
@@ -23,7 +20,7 @@ export default class Add implements CommandModule {
         this.builder = (yargs: Argv) => {
             return yargs
                 .positional('name', {
-                    describe: 'name of your project',
+                    describe: 'name of your component',
                     type: 'string',
                 });
         }
@@ -31,11 +28,22 @@ export default class Add implements CommandModule {
         this.handler = async (args: Arguments) => {
             // TODO: check if pwd is a Floyd app
 
+            // add component name to app.config.json
+            let componentName = args.name as string;
+            let fileName = join(__dirname, 'app.config.json');
+            
+            let config = require(fileName);
+            config.components.push(componentName);
 
-            // component name
-            let name = args.name as string;
+            writeFile(fileName, JSON.stringify(config, null, 2), (err) => {
+                if (err) {
+                    chalk.red(err.message);
+                    process.exit(1);
+                }
+            });
 
-            // add name to app.config.js
+            // finally inform user
+            console.log(chalk.green(`Successfully created component ${componentName}.`));
         }
     }
 };
