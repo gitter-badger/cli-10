b22f1e92109053dd6599fbd86579e51cfe5b1674
Syed Faraaz Ahmad <faraaz98@live.com>
2019-07-07 23:56:03 +0530

    fix postinstall script for already cloned repo

--- postinstall.sh
+++ postinstall.sh
@@ -3,7 +3,10 @@
 date_today="$(date "+%d_%m_%Y")"
 
 # create folder ~/.floyd/app/<date>
-mkdir -p "$HOME/.floyd/app/$date_today"
+DIRECTORY="$HOME/.floyd/app/$date_today"
 
 # clone the app in the directory
-git clone https://github.com/floyd-framework/app.git "$HOME/.floyd/app/$date_today"
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir -p "$DIRECTORY"
+    git clone https://github.com/floyd-framework/app.git "$DIRECTORY"
+fi
